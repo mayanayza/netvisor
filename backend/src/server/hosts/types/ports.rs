92f1c5b068a99de51cf1e910ee06e3baed4f1551
Michelfrancis Bustillos <michelfrancisb@gmail.com>
2025-11-04 11:56:51 -0600

    Fix RTSP Capitalization

--- backend/src/server/hosts/types/ports.rs
+++ backend/src/server/hosts/types/ports.rs
@@ -71,7 +71,7 @@ pub enum PortBase {
     HttpAlt,
     Https,
     HttpsAlt,
-    RTSP,
+    Rtsp,
     Custom(PortConfig),
 }
 
@@ -274,7 +274,7 @@ impl PortBase {
                 number: 8443,
                 protocol: TransportProtocol::Tcp,
             },
-            PortBase::RTSP => PortConfig {
+            PortBase::Rtsp => PortConfig {
                 number: 554,
                 protocol: TransportProtocol::Tcp,
             },
@@ -314,13 +314,13 @@ impl TypeMetadataProvider for PortBase {
             PortBase::Snmp => "SNMP",
             PortBase::Rdp => "RDP",
             PortBase::Ntp => "NTP",
-            PortBase::Rtsp => "RTSP",
+            PortBase::Rtsp => "Rtsp",
             PortBase::Dhcp => "DHCP",
             PortBase::Http => "HTTP",
             PortBase::HttpAlt => "HTTP Alt",
             PortBase::Https => "HTTPS",
             PortBase::HttpsAlt => "HTTPS Alt",
-            PortBase::RTSP => "RTSP",
+            PortBase::Rtsp => "RTSP",
             PortBase::Custom(_) => "Custom",
         }
     }
@@ -345,7 +345,7 @@ impl TypeMetadataProvider for PortBase {
             PortBase::HttpAlt => "Alternative HTTP Port",
             PortBase::Https => "Hypertext Transfer Protocol Secure",
             PortBase::HttpsAlt => "Alternative HTTPS Port",
-            PortBase::RTSP => "Real-Time Streaming Protocol",
+            PortBase::Rtsp => "Real-Time Streaming Protocol",
             PortBase::Custom(_) => "Custom Port Configuration",
         }
     }
